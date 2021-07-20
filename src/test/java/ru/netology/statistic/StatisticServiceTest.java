d971020ea58f4e424f5e9a98901ca81ec2486e9b
FingRinger <ard21study@gmail.com>
2021-07-20 12:09:24 +0300

    CheckStyle plugin is added

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -1,2 +1,19 @@
-package ru.netology.statistic;public class StatisticServiceTest {
+package ru.netology.statistic;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {3, 5, 8, 4, 5, 12, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
